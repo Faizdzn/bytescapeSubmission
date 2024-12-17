e3746858986fff1d9b596d8c55986b4354f7bb20
Faizdzn <masterfrostgt@gmail.com>
2024-12-17 12:24:16 +0000

    v

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,4 +1,4 @@
-<div class="snap-center shadow-md shadow-black_1/25 min-w-[275px] flex p-4 rounded-lg bg-white_1 border-2 border-black_1/10 flex-col gap-2">
+<div class="snap-center shadow-md shadow-black_1/25 md:max-w-[275px] min-w-[275px] flex p-4 rounded-lg bg-white_1 border-2 border-black_1/10 flex-col gap-2">
     <div class="bg-black/25 rounded-md w-full h-[100px] bg-center bg-[url('{{$course['course_thumb']}}')] bg-cover"></div>
     <div class="flex gap-2 justify-between">
         <div class="flex flex-col">
@@ -13,7 +13,7 @@
             {{$star}}
         </div>
     </div>
-    <div class="flex gap-2 mt-4">
+    <div class="flex gap-2 mt-auto">
         <x-button url="/course/{{$cid}}">
             <i class="ti ti-external-link"></i>
             <span>Kunjungi Course</span>
