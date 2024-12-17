528e2928fb2a608856186d8c353bdc713357bced
Faizdzn <masterfrostgt@gmail.com>
2024-12-17 13:15:23 +0000

    d

--- public/js/service.js
+++ public/js/service.js
@@ -45,24 +45,24 @@ const register = async () => {
 }
 
 const searchKelas = () => {
-    const pg = document.getElementById("kelas-page-val").getAttribute("value");
+    // const pg = document.getElementById("kelas-page-val").getAttribute("value");
     const dd = document.getElementById("kelas-dd-val").getAttribute("value");
     const sv = document.getElementById("search_norm-val").value;
-    window.location.href = `/kelas?search=${sv}&sort=${dd}&page=${pg}`;
+    window.location.href = `/kelas?search=${sv}&sort=${dd}&page=1`;
 }
 
 const searchCourseInKelas = (id) => {
-    const pg = document.getElementById("course-page-val").getAttribute("value");
+    // const pg = document.getElementById("course-page-val").getAttribute("value");
     const dd = document.getElementById("course-dd-val").getAttribute("value");
     const sv = document.getElementById("search_course-val").value;
-    window.location.href = `/kelas/${id}?search=${sv}&sort=${dd}&page=${pg}`;
+    window.location.href = `/kelas/${id}?search=${sv}&sort=${dd}&page=1`;
 }
 
 const searchCourse = () => {
-    const pg = document.getElementById("course-page-val").getAttribute("value");
+    // const pg = document.getElementById("course-page-val").getAttribute("value");
     const dd = document.getElementById("course-dd-val").getAttribute("value");
     const sv = document.getElementById("search_course-val").value;
-    window.location.href = `/course?search=${sv}&sort=${dd}&page=${pg}`;
+    window.location.href = `/course?search=${sv}&sort=${dd}&page=1`;
 }
 
 const changePass = async(cookie) => {
